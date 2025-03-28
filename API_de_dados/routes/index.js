c4df36d39e427ceca2c28dc298ceecc367d6a2e1
rafael-vilasboas <rafaelvilasboas0102@gmail.com>
2025-03-28 21:58:35 +0000

    Scrapper todos os produtos

--- API_de_dados/routes/index.js
+++ API_de_dados/routes/index.js
@@ -4,22 +4,32 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  const url = 'https://www.continente.pt/mercearia/cafe-cha-e-bebidas-soluveis';
-  axios.get(url)
-    .then(resp => {
-      const $ = cheerio.load(resp.data);
-      let precos = []
-      $('span.ct-price-formatted').each((index, element) => {
-        precos.push($(element).text().trim());
-        console.log($(element).text());
-      });
-      res.render('index', { title: 'Express', precos:  precos});
-    })
-    .catch(error => {
-      console.log(error)
-      res.render('error', {error: error})
-    })
+router.get('/', async function(req, res, next) {
+  step = 36
+//  total = 5422
+  total = 100
+  start = 0
+  let url = `https://www.continente.pt/mercearia/?start${start}`;
+  let produtos = []
+
+  while (start < total) {
+    url = `https://www.continente.pt/mercearia/?start${start}`;
+    await axios.get(url)
+      .then(resp => {
+        const $ = cheerio.load(resp.data);
+        $('.product').each((index, element) => {
+          const product = $(element).find('div').attr('data-product-tile-impression');
+          produtos.push(product);
+          console.log(product);
+        });
+      })
+      .catch(error => {
+        console.log(error)
+        res.render('error', {error: error})
+      })
+    start += step;
+  }
+  res.render('index', { title: 'Express', produtos:  produtos});
 });
 
 module.exports = router;
