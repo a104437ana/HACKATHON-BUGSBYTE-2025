209301714de6e76498d9f7f0ad0c03290c8e7835
rafael-vilasboas <rafaelvilasboas0102@gmail.com>
2025-03-29 10:29:00 +0000

    Adicionado api produtos e produto ao json server

--- API_de_dados/routes/scrapper.js
+++ API_de_dados/routes/scrapper.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/continente', async function(req, res, next) {
   step = 36
 //  total = 5422
   total = 100
@@ -18,9 +18,17 @@ router.get('/', async function(req, res, next) {
       .then(resp => {
         const $ = cheerio.load(resp.data);
         $('.product').each((index, element) => {
-          const product = $(element).find('div').attr('data-product-tile-impression');
-          produtos.push(product);
-          console.log(product);
+          const productStr = $(element).find('div').attr('data-product-tile-impression');
+
+          if (productStr) {
+            try {
+              const product = JSON.parse(productStr);
+              produtos.push(product);
+            }
+            catch (error) {
+//              console.log(`Erro ao fazer parse do objeto: ${productStr}`);
+            }
+          }
         });
       })
       .catch(error => {
@@ -29,7 +37,32 @@ router.get('/', async function(req, res, next) {
       })
     start += step;
   }
-  res.render('index', { title: 'Express', produtos:  produtos});
+//  res.render('index', { title: 'Express', produtos:  produtos});
+  res.status(200).jsonp(produtos)
+});
+
+// GET PRODUCTS
+router.get('/produtos', function(req, res, next) {
+  axios.get(`http://localhost:3000/products_info`)
+  .then(resp => {
+    res.status(200).jsonp(resp.data)
+  })
+  .catch(error => {
+    console.log(error)
+    res.render('error', {error: error})
+  })
+});
+
+// GET PRODUCT
+router.get('/produtos/:id', function(req, res, next) {
+  axios.get(`http://localhost:3000/products_info?sku=${req.params.id}`)
+  .then(resp => {
+    res.status(200).jsonp(resp.data)
+  })
+  .catch(error => {
+    console.log(error)
+    res.render('error', {error: error})
+  })
 });
 
 module.exports = router;
