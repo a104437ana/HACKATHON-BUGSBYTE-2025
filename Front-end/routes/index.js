dbdb33befd3c9e41e55aca6e7a5d5ece75c6b033
a104437ana <a104437@alunos.uminho.pt>
2025-03-29 18:17:52 +0000

    corrigir

--- Front-end/routes/index.js
+++ Front-end/routes/index.js
@@ -14,14 +14,6 @@ router.get('/produtos', function(req, res, next) {
   .then(resp => res.status(200).render("produtos", {title: "Produtos", date: date, suggestions: resp.data}))
 });
 
-router.get('/produtos/:id',  function(req, res, next) {
-  var product = req.params.id;
-  var date = new Date().toLocaleString('pt-PT', { hour12: false });
-  axios.get(`http://localhost:3001/produtos/${product}`)
-  .then(resp => {res.status(200).render("produto", {title: "Produto " + resp.data["product_dsc"], nome: resp.data["product_dsc"], id: resp.data["sku"], date: date, suggestions: [], prices : [{store: "Continente", price: resp.data["20231226"]}]})
-  console.log(resp.data["20231226"])});
-});
-
 router.get('/cabazes', async function(req, res, next) {
   var date = new Date().toLocaleString('pt-PT', { hour12: false });
   let produtos = [6927230, 5254224, 6654470, 2456648, 6022110, 6927230, 3795692]
@@ -46,8 +38,10 @@ router.get('/cabazes', async function(req, res, next) {
 router.post('/search', (req, res) => {
   var date = new Date().toLocaleString('pt-PT', { hour12: false });
   const suggestionObject = JSON.parse(req.body.search); 
-  const product = suggestionObject.sku;
-  res.redirect(`/produtos/${product}`);
+  const product = suggestionObject.id;
+  axios.get(`http://localhost:3001/produtos/${product}`)
+  .then(resp => {res.status(200).render("produto", {title: "Produto " + resp.data["product_dsc"], nome: resp.data["product_dsc"], id: resp.data["id"], date: date, suggestions: [], prices : [{store: "Continente", price: resp.data["20231226"]}]})
+  console.log(resp.data["20231226"])});
 });
 
 router.get('/atualizaprecos', async function(req, res, next) {
