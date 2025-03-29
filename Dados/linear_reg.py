c3dcccab85fd2750dd98b3501af144be9e190205
rafael-vilasboas <rafaelvilasboas0102@gmail.com>
2025-03-29 20:13:30 +0000

    Adicionado polinomios de grau

--- Dados/linear_reg.py
+++ Dados/linear_reg.py
@@ -44,7 +44,14 @@ def csv_reg(csv_f, separator, id, value, count):
             print(f"Intercepto (b): {dict_linear[key][0]}")
             print(f"Coeficiente angular (m): {dict_linear[key][1][0]}")
 
+def eval(x, polynomial):
+    return np.polyval(polynomial, x)
+
+def eval_derivative(x, polynomial_derivate):
+    return np.polyval(polynomial_derivate, x)
+
 def csv_reg_pol(csv_f, separator, id, value, count, degree):
+
     with open(csv_f, 'r', encoding='utf-8') as f:
         csv_reader = csv.DictReader(f, delimiter=separator)
         dict = {}
@@ -57,7 +64,7 @@ def csv_reg_pol(csv_f, separator, id, value, count, degree):
                 dict[id_line] = []
             dict[id_line].append((float(value_line), float(count_line)))
 
-        dict_linear = {}
+        dict_polynomial = {}
 
         for key in dict.keys():
             x = []
@@ -72,12 +79,53 @@ def csv_reg_pol(csv_f, separator, id, value, count, degree):
             max_x = res.x[0]
             max_y = polynomial(max_x)
 
-            dict_linear[key] = (max_x, max_y)
+            polynomial_derivate = np.polyder(polynomial)
+            critical_points = np.roots(polynomial_derivate)
+            critical_points = critical_points[np.isreal(critical_points)]
+            critical_point = critical_points[0].real
+            critical_y = polynomial(critical_point)
 
-        for key in dict_linear.keys():
+#            res_der = minimize(lambda x: eval_derivative(x, polynomial_derivate), x0=0, bounds=[(0.6, 2)])
+            res_der = minimize(lambda x: eval_derivative(x, polynomial_derivate), x0=0, bounds=[(0, 6)])
+            min_x_der = res_der.x[0]
+            min_y_der = eval_derivative(min_x_der, polynomial_derivate)
+
+            dict_polynomial[key] = (max_x, max_y, critical_point, critical_y, min_x_der, min_y_der)
+
+        for key in dict_polynomial.keys():
             print(f'Id: {key}')
-            print(f"max x: {dict_linear[key][0]}")
-            print(f"max y: {dict_linear[key][1]}")
+            print(f"max x: {dict_polynomial[key][0]}")
+            print(f"max y: {dict_polynomial[key][1]}")
+            print(f"der x: {dict_polynomial[key][2]}")
+            print(f"der y: {dict_polynomial[key][3]}")
+            print(f"mxd x: {dict_polynomial[key][4]}")
+            print(f"mxd y: {dict_polynomial[key][5]}")
+
+        plt.figure(figsize=(10, 6))
+
+        x_vals = []
+        y_vals_polynomial = []
+        y_vals_derivative = []
+        for i in range(0,100):
+            x_vals.append(i/10)
+            y_vals_polynomial.append(eval(i/10, polynomial))
+            y_vals_derivative.append(eval_derivative(i/10, polynomial_derivate))
+
+        plt.plot(x_vals, y_vals_polynomial, label="Polinômio", color='b', linestyle='-', linewidth=2)
+
+        plt.plot(x_vals, y_vals_derivative, label="Derivada", color='r', linestyle='--', linewidth=2)
+
+        # Adicionar título e legendas
+        plt.title("Gráfico do Polinômio e Sua Derivada")
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.axhline(0, color='black',linewidth=1)  # Linha horizontal (eixo x)
+        plt.axvline(0, color='black',linewidth=1)  # Linha vertical (eixo y)
+        plt.legend()
+
+        # Mostrar o gráfico
+        plt.grid(True)
+        plt.show()
 
 csv_f = sys.argv[1]         # ficheiro csv
 separator = sys.argv[2]
@@ -85,5 +133,5 @@ id = sys.argv[3]
 value = sys.argv[4]
 count = sys.argv[5]
 
-csv_reg(csv_f, separator, id, value, count)
-csv_reg_pol(csv_f, separator, id, value, count, 2)
+#csv_reg(csv_f, separator, id, value, count)
+csv_reg_pol(csv_f, separator, id, value, count, 3)
