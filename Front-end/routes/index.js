83a2ab44e467dd85ba44f1517c5bff7fb7ab0271
a104263 <a104263@alunos.uminho.pt>
2025-03-29 12:26:41 +0000

    Correção

--- Front-end/routes/index.js
+++ Front-end/routes/index.js
@@ -19,21 +19,14 @@ router.get('/cabazes', function(req, res, next) {
   res.status(200).render("cabazes", {title: "Cabazes", date: date});
 });
 
-function getPriceForProduct(productName) {
-  return [
-    { store: 'Loja OI', competitor: 'A', price: 100 },
-    { store: 'Loja OI', competitor: 'B', price: 95 },
-    { store: 'Loja XYZ', competitor: 'A', price: 110 },
-  ];
-}
-
 
 router.post('/search', (req, res) => {
   var date = new Date().toLocaleString('pt-PT', { hour12: false });
-  const productName = req.body.search;
-  const prices = getPriceForProduct(productName);
-  console.log(prices)
-  res.status(200).render("produtos", {title: "Produtos", date: date, suggestions: ['Arroz', 'Leite'], prices : prices});
+  console.log(req.body)
+  const suggestionObject = JSON.parse(req.body.search); 
+  const product = suggestionObject.sku;
+  axios.get(`http://localhost:3001/produtos/${product.sku}`)
+  .then(resp => res.status(200).render("produtos", {title: "Produtos", date: date, suggestions: [], prices : resp.data["20231226"]}));
 });
 
 module.exports = router;
