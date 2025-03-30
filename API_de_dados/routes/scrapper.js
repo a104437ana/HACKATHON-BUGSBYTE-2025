1fcf578a882f33e5ce0937adfa871f90f35f0896
rafael-vilasboas <rafaelvilasboas0102@gmail.com>
2025-03-30 10:46:42 +0100

    Corrigidas funções não utilizadas

--- API_de_dados/routes/scrapper.js
+++ API_de_dados/routes/scrapper.js
@@ -6,7 +6,7 @@ const xml2js = require('xml2js');
 const natural = require('natural');
 
 
-/* GET home page. */
+// GET precos continente
 router.get('/continente', async function(req, res, next) {
   let url = "https://www.continente.pt/sitemap-custom_sitemap_1-product.xml";
   let lista_urls = [];
@@ -14,15 +14,12 @@ router.get('/continente', async function(req, res, next) {
 
   axios.get('http://localhost:3000/products_info')
     .then(response => {
-      // Acessar o array de objetos e mapear para pegar o id de cada objeto
       ids = response.data.map(product => String(product.id).trim());
-      console.log("IDs:", ids);  // Verifique os ids recebidos do JSON Server
     })
     .catch(error => {
       console.error('Erro ao buscar os dados:', error);
     });
 
-  // Aguardar o carregamento dos IDs
   await axios.get(url)
     .then(response => {
       const parser = new xml2js.Parser();
@@ -31,7 +28,6 @@ router.get('/continente', async function(req, res, next) {
           console.error("Erro ao parsear o XML:", err);
           return;
         }
-        // Acessando as URLs dentro da estrutura XML convertida
         const urls = result.urlset.url;
         urls.forEach(item => {
           if (item.loc && item.loc[0]) {
@@ -47,24 +43,18 @@ router.get('/continente', async function(req, res, next) {
   let produtos = [];
   let i = 0;
   for (let url of lista_urls) {
-    console.log(`${i}/${lista_urls.length}`);  // Mostrar qual URL está sendo processada
+    console.log(`${i}/${lista_urls.length}`);
     i += 1;
 
     const idMatch = url.match(/(\d+).html/);
     let productId = 0;
     if (idMatch) {
       productId =  String(idMatch[1]).trim();
-      //console.log(productId)
     }
 
-    //console.log(`Extrair ID: ${productId}`);  // Verificar o ID extraído da URL
-
     if (productId && ids.includes(productId)) {
-      console.log("ID encontrado na lista de IDs!");  // Confirmação de que está no `if`
-
       await axios.get(url)
         .then(resp => {
-          console.log("Entrei no if");
           const $ = cheerio.load(resp.data);
           const productName = $('.pwc-h3.col-h3.product-name.pwc-font--primary-extrabold.mb-0')
             .text().trimStart().replace(/^\s/, '').trimEnd().replace(/\s$/, '');
@@ -87,7 +77,6 @@ router.get('/continente', async function(req, res, next) {
           console.error("Erro ao acessar a URL do produto:", error);
         });
     } else {
-      //console.log("ID não encontrado na lista de IDs ou não corresponde.");
     }
   }
 
@@ -108,48 +97,6 @@ router.get('/continente', async function(req, res, next) {
 
   res.status(200).send("Preços atualizados");
 })
-
-router.get('/continente/:id', async function(req, res, next) {
-  start = req.params.id
-  let url = `https://www.continente.pt/mercearia/?start${start}`;
-  let produtos = []
-
-  await axios.get(url)
-    .then(resp => {
-      const $ = cheerio.load(resp.data);
-      $('.product').each((index, element) => {
-        const productStr = $(element).find('div').attr('data-product-tile-impression');
-
-        if (productStr) {
-          try {
-            const product = JSON.parse(productStr);
-            produtos.push(product);
-          }
-          catch (error) {
-          }
-        }
-      });
-    })
-    .catch(error => {
-      console.log(error)
-      res.render('error', {error: error})
-    })
-  for (let produto of produtos) {
-      let id = parseInt(produto.id)
-      try {
-        const resp = await axios.get(`http://localhost:3000/products_info/${id}`)
-        let prod = resp.data
-        if (prod) {
-          prod["product_price"] = produto.price
-          console.log(id)
-          await axios.put(`http://localhost:3000/products_info/${id}`, prod)
-        }
-      }
-      catch (error) {
-      }
-  }
-  res.status(200).send("")
-});
 
 // GET PRODUCTS
 router.get('/produtos', function(req, res, next) {
@@ -216,13 +163,11 @@ router.get('/produtos_info/:id', function(req, res, next) {
   })
 });
 
+// GET precos mini preco
 router.get('/minipreco/mercearia', async function(req, res, next) {
   step = 1
   total_category = {0:183, 1:6, 3:183, 5:14, 6:10, 7:26, 8:8, 9:35, 10:3, 11:18, 12:2}
-//  total = 1
   start = 0
-//  let url = `https://www.minipreco.pt/produtos/mercearia/c/WEB.0${String(category).padStart(2, '0')}.000.00000?q=%3Arelevance&page=${start}&disp=2000`;
-//  let produtos = []
   let id = 0
 
   const resp = await axios.get(`http://localhost:3000/products_info`)
@@ -245,7 +190,6 @@ router.get('/minipreco/mercearia', async function(req, res, next) {
           const productName = $(element).find('.details').text().trimStart().replace(/^\s/, '');
           const productPrice = $(element).find('.price').text().trimStart().replace(/^\s/, '').replace(/€/, '').trimEnd().replace(/\s$/, '');
 
-          console.log(id)
           if (productName && productPrice) {
             let bestMatch = null
             let highestScore = 0
@@ -263,13 +207,8 @@ router.get('/minipreco/mercearia', async function(req, res, next) {
                 const respContinente = await axios.get(`http://localhost:3000/products_info/${id_continente}`)
                 let product = respContinente.data
                 product["preco_minipreco"] = productPrice
-    //            const product_obj = {"id": id, "product_dsc": productName, "product_price": productPrice}
-    //            const productStr = JSON.stringify(product_obj);
-    //            const product = JSON.parse(productStr)
-    //            console.log(product)
                 await axios.put(`http://localhost:3000/products_info/${id_continente}`, product)
                 console.log(product)
-    //            produtos.push(product);
               }
               catch (error) {
                 console.log("Erro: " + productName + " " + highestScore)
@@ -287,34 +226,9 @@ router.get('/minipreco/mercearia', async function(req, res, next) {
       start += step;
     }
     start = 0
-  }/*
-  produtosMinipreco = {
-    "minipreco": produtos
   }
-  await axios.post(`http://localhost:3000/competicao`, produtosMinipreco)
-    .then(resp => {
-      console.log("Post realizado com sucesso")
-      res.status(201).send("Post realizado com sucesso")
-    })
-    .catch(error => {
-      console.log(error)
-      res.render('error', {error: error})
-    })*/
    res.status(200).send("")
 });
 
-router.get('/minipreco/:id', async function(req, res, next) {
-});
-
-router.get('/minipreco/continente', async function(req, res, next) {
-  axios.get(`http://localhost:3000/products_info`)
-  .then(resp => {
-    res.status(200).jsonp(resp.data[0])
-  })
-  .catch(error => {
-    console.log(error)
-    res.render('error', {error: error})
-  })
-});
 
 module.exports = router;
