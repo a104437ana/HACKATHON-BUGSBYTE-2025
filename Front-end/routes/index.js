a2f061749b1839c0f027e15abcc1ef38412c92be
a104263 <a104263@alunos.uminho.pt>
2025-03-30 11:05:21 +0100

    Alterações

--- Front-end/routes/index.js
+++ Front-end/routes/index.js
@@ -35,7 +35,7 @@ router.get('/produtos/:id',function(req,res,next) {
           reco = x
         }
         console.log(reco)
-        res.status(200).render("produto", {reco: reco, title: "Produto " + resp.data["product_dsc"], nome: resp.data["product_dsc"], id: resp.data["id"], date: date, suggestions: [], pa : resp.data['product_price'], prices : [{store: "Continente", price: resp.data["20231226"]}
+        res.status(200).render("produto", {reco: reco, title: "Produto " + resp.data["product_dsc"], nome: resp.data["product_dsc"], id: resp.data["id"], date: date, suggestions: [],index : resp.data["20231226"], prices : [{store: "Continente", price: resp.data["product_price"]}, {store: "Pingo Doce", price: resp.data["pingo_doce_price"]}, {store: "Mercadona", price: resp.data["mercadona_price"]}, {store: "Minipreço", price: resp.data["minipreco_price"]}
       ]})}
     console.log(resp.data["20231226"])});
 })
@@ -44,7 +44,10 @@ router.get('/cabazes', async function(req, res, next) {
   var date = new Date().toLocaleString('pt-PT', { hour12: false });
   let produtos = [6927230, 5254224, 6654470, 2203031, 3795692, 7825575, 2816831, 6919055, 7579107, 5038799, 7403557, 4636366, 7848011, 2597619]
   let indice_cabaz = 0
-  let total_cabaz = 0
+  let total_cabaz_Cont = 0
+  let total_cabaz_Pingo_Doce = 0
+  let total_cabaz_Mercadona = 0
+  let total_cabaz_Minipreco = 0
   let i = 0
   let lista = []
   let ll = []
@@ -56,14 +59,16 @@ router.get('/cabazes', async function(req, res, next) {
         ll.push(resp.data["id"])
         indice_cabaz += resp.data["20231226"];
         if (resp.data["product_price"]) {
-          total_cabaz += parseFloat(resp.data["product_price"]);
+          total_cabaz_Cont += parseFloat(resp.data["product_price"]);
+          total_cabaz_Pingo_Doce += parseFloat(resp.data["pingo_doce_price"]);
+          total_cabaz_Mercadona += parseFloat(resp.data["mercadona_price"]);
+          total_cabaz_Minipreco += parseFloat(resp.data["minipreco_price"]);
         }
         i += 1
       });
   }
     indice_cabaz = indice_cabaz/i
-    console.log(total_cabaz)
-      res.status(200).render("cabazes", {title: "Cabazes", date: date, ll: ll, lista: lista, cabazes: [{name: "Cabaz essencial", indice: indice_cabaz, total: total_cabaz.toFixed(2)}]})
+      res.status(200).render("cabazes", {title: "Cabazes", date: date, ll: ll, lista: lista, cabazes: [{name: "Cabaz essencial", indice: indice_cabaz, totalCont: total_cabaz_Cont.toFixed(2), totalMercadona: total_cabaz_Mercadona.toFixed(2), totalPingo_Doce: total_cabaz_Pingo_Doce.toFixed(2), total_Minipreco: total_cabaz_Minipreco.toFixed(2)}]})
   }); 
 
 
