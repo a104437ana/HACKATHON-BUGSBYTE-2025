bb7c7b7d80ab2ec2d30e08a2e5becef9b1650890
a104437ana <a104437@alunos.uminho.pt>
2025-03-30 02:20:31 +0100

    clean

---
+++ Dados/clean.py
@@ -0,0 +1,22 @@
+import json
+
+# Nome do arquivo JSON
+filename = "dados.json"
+
+# Carregar os dados do arquivo
+with open(filename, "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+# Filtrar os objetos que possuem a chave 'product_price'
+filtered_products = [
+    product for product in data["products_info"] if "product_price" in product
+]
+
+# Atualizar os dados com a lista filtrada
+data["products_info"] = filtered_products
+
+# Salvar os dados filtrados de volta no mesmo arquivo
+with open(filename, "w", encoding="utf-8") as file:
+    json.dump(data, file, indent=2, ensure_ascii=False)
+
+print("Arquivo atualizado com sucesso!")
